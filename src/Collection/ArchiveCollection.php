e3fbd7b64e9e8893795288b78b77063d475ed13c
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-09-23 15:18:12 +0200

    refactor ArchiveCollection: change archiveTypes to be resolved from iterable, improve lazy loading of archive types

--- src/Collection/ArchiveCollection.php
+++ src/Collection/ArchiveCollection.php
@@ -4,20 +4,35 @@ namespace HeimrichHannot\MediaLibraryBundle\Collection;
 
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
-readonly class ArchiveCollection
+class ArchiveCollection
 {
+    private array $archiveTypes;
+
     public function __construct(
         #[TaggedIterator('huh.media_library.archive_type', defaultIndexMethod: 'getAlias')]
-        private iterable $archiveTypes,
+        private readonly iterable $archiveTypesIterable,
     ) {}
+
+    private function resolve(): array
+    {
+        if (!isset($this->archiveTypes))
+        {
+            foreach ($this->archiveTypesIterable as $alias => $service)
+            {
+                $this->archiveTypes[$alias] = $service;
+            }
+        }
+
+        return $this->archiveTypes;
+    }
 
     public function getAll(): iterable
     {
-        return $this->archiveTypes;
+        return $this->resolve();
     }
 
     public function get(string $alias): ?object
     {
-        return $this->archiveTypes[$alias] ?? null;
+        return $this->resolve()[$alias] ?? null;
     }
 }
