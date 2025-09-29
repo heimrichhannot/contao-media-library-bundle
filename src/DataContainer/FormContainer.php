6d358865514992ee790c555a88312309f26ab968
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-09-29 15:01:37 +0200

    refactor FormContainer: update callback annotation to use attributes and improve archive options retrieval

--- src/DataContainer/FormContainer.php
+++ src/DataContainer/FormContainer.php
@@ -1,29 +1,22 @@
 <?php
 
-/*
- * Copyright (c) 2023 Heimrich & Hannot GmbH
- *
- * @license LGPL-3.0-or-later
- */
-
 namespace HeimrichHannot\MediaLibraryBundle\DataContainer;
 
-use Contao\CoreBundle\ServiceAnnotation\Callback;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use HeimrichHannot\MediaLibraryBundle\Model\ArchiveModel;
 
 class FormContainer
 {
-    /**
-     * @Callback(table="tl_form", target="fields.ml_archive.options")
-     */
-    public function onFieldsMlArchiveOptionsCallback(): array
-    {
-        $options = [];
-        $archives = ArchiveModel::findAll();
+    public const TABLE = 'tl_form';
 
-        if (null === $archives) {
-            return $options;
+    #[AsCallback(self::TABLE, 'fields.ml_archive.options')]
+    public function getMediaLibraryArchiveOptions(): array
+    {
+        if (!$archives = ArchiveModel::findAll()) {
+            return [];
         }
+
+        $options = [];
 
         foreach ($archives as $archive) {
             $options[$archive->id] = $archive->title;
