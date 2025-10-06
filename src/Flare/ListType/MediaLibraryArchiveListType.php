2cda845093a1253351a59626643e52d40139edb8
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-10-06 15:39:59 +0200

    add prepareQuery method to configure query with inner join and exception handling

--- src/Flare/ListType/MediaLibraryArchiveListType.php
+++ src/Flare/ListType/MediaLibraryArchiveListType.php
@@ -4,10 +4,12 @@ namespace HeimrichHannot\MediaLibraryBundle\Flare\ListType;
 
 use HeimrichHannot\FlareBundle\DependencyInjection\Attribute\AsListCallback;
 use HeimrichHannot\FlareBundle\DependencyInjection\Attribute\AsListType;
+use HeimrichHannot\FlareBundle\Exception\FlareException;
 use HeimrichHannot\FlareBundle\FilterElement\PublishedElement;
 use HeimrichHannot\FlareBundle\List\ListQueryBuilder;
 use HeimrichHannot\FlareBundle\List\PresetFiltersConfig;
 use HeimrichHannot\FlareBundle\ListType\AbstractListType;
+use HeimrichHannot\MediaLibraryBundle\EventListener\Flare\JoinCodefogTagsCallback;
 
 #[AsListType(alias: self::TYPE, dataContainer: 'tl_ml_item', palette: '{archive_legend},ml_archive')]
 class MediaLibraryArchiveListType extends AbstractListType
@@ -15,6 +17,14 @@ class MediaLibraryArchiveListType extends AbstractListType
     public const TYPE = 'ml_archive';
     public const ALIAS_ARCHIVE = 'ml_archive';
 
+    /**
+     * Configures the query by preparing an inner join on the specified table.
+     *
+     * @param ListQueryBuilder $builder The query builder instance used for constructing the query.
+     * @throws FlareException if there is an error during query preparation.
+     * @see JoinCodefogTagsCallback for joining tags.
+     * @internal This method is intended for internal use only and should not be called directly.
+     */
     #[AsListCallback(self::TYPE, 'query.configure')]
     public function prepareQuery(ListQueryBuilder $builder): void
     {
